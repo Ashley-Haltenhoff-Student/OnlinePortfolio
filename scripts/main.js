da5b358db890f602744f80d058f7bdb41155c320
AJ <114190202+crustylemons@users.noreply.github.com>
2024-10-08 02:02:33 +0000

    created sidebar menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,14 @@
+const sidebar = document.getElementById('sidebar-menu');
+const main_content = document.getElementById('all-content');
 
-let newX = 0, newY = 0, startX = 0, startY = 0, mouseX = 0, mouseY = 0;
+function closeSidebar() {
+    sidebar.style.width = '0'
+    sidebar.style.padding = '0'
+    main_content.style.filter = "brightness(1)"
+}
 
-const windows = document.getElementsByClassName('window');
-
-addEventListener('mousemove', (e) => {
-    mouseX = e.clientX
-    mouseY = e.clientY
-});
-
-windows.forEach(window => {
-    window.addEventListener('dragstart', () => {
-        console.log('dragstart')
-        let position = window.getBoundingClientRect()
-        startX = position.left
-        startY = position.top
-    })
-    window.addEventListener('dragend',() => {
-        console.log('dragend')
-        newX = startX - mouseX
-        newY = startY - mouseY
-
-        window.position = newX, newY
-    })
-})
+function openSidebar() {
+    sidebar.style.width = '400px';
+    sidebar.style.padding = '12px'
+    main_content.style.filter = "brightness(0.75)"
+}
